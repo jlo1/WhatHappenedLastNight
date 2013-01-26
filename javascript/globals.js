f9ece76085af2a3b2992424d9a878cf6e51eb50f
unknown <Justin@Justin-PC.(none)>
2013-01-26 09:26:20 -0500

    Implement the nav bar

--- javascript/globals.js
+++ javascript/globals.js
@@ -12,3 +12,9 @@ CUR_EVENT_ITEMS = [new Item(14, 8, 3, "3:00", 0, "The first ever message. Crazy 
 /* If an event is being looked at, store the object that contains its information.
  * This is used when a user adds a comment or an item */
 CUR_EVENT = new Event(8, "Tartan Hacks", "The party going down at Tartan Hacks", "1-1-2010", "1-2-2010", "fb1", "CHANGE", CUR_EVENT_ITEMS);
+
+MAX_NAV_SIZE = 500;
+
+
+/* Specify the name of the ID of the current page */
+CUR_NAV_ID = "login-page";
--- javascript/main.js
+++ javascript/main.js
@@ -3,12 +3,37 @@ $(document).ready(function() {
 		login();
 	});
 	
-	$("
+	$(".navBar ul li").bind("click", function() {
+		$(".navBar ul li").animate({maxWidth: 200}, 500);
+		$(this).animate({maxWidth: MAX_NAV_SIZE}, 1000);
+		
+		var newID = $(this).attr("class"); 
+		switch(newID)
+			{
+			case "calendar":
+			  switchPages("event-page");
+			  break;
+			case "nights":
+			  switchPages("party-page");
+			  break;
+			//Case logout
+			default:
+			  //Do SOmething special
+			}
+	});
 });
 
 function switchPages(targetPageID) {
+	//Don't do anything if they want to navigate to the same page.
+	if (targetPageID == CUR_NAV_ID) return;
+	
+	if (targetPageID == "login-page") $(".navBar").addClass("hideMe");
+	
+	if (CUR_NAV_ID == "login-page") $(".navBar").removeClass("hideMe");
+	
 	$(".page").removeClass("active");
 	
 	var targetPage = "#" + targetPageID;
 	$(targetPage).addClass("active");
+	CUR_NAV_ID = targetPageID;
 }
