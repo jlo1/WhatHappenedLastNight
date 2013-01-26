457011f56f2842cb90cce607ebc47d7f82ddd60c
Shreepal Shah <shreepal13@gmail.com>
2013-01-26 00:30:17 -0500

    added javascript files to index.html

--- javascript/graph_api_queries.js
+++ javascript/graph_api_queries.js
@@ -3,7 +3,7 @@
    
    
 // Get all attendees of an event
-$.getJSON("https://graph.facebook.com/331218348435/invited?access_token=" + CURR_USER.access_token + "&callback=?", function(response) {
+/*$.getJSON("https://graph.facebook.com/331218348435/invited?access_token=" + CURR_USER.access_token + "&callback=?", function(response) {
   $(response.data).each(function() {
     console.log(this.name);
   });
@@ -16,3 +16,4 @@ $.getJSON("https://graph.facebook.com/331218348435/invited?access_token=" + CURR
 		"AAACEdEose0cBAJG9ZA3xmcpxZC5x4gJwJzwKyyJlAze1ZAII2P83o85pQE60uiZCLUSL6weZCtrXZCKmYlSgcqU95L16ZBSZARPa3a3UMg2DzQZDZD&callback=?", function(response) {
 					console.log(response);
 			});
+*/
